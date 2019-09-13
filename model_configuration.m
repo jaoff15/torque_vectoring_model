c70b534fb50951f4c09dcb7d93ed97f5c5657670
jaoff15 <jaoff15@student.sdu.dk>
2019-09-13 15:26:04 +0200

    Longitudinal weight transfer added. Calculated weight transfers also changed to normalized weights

--- model_configuration.m
+++ model_configuration.m
@@ -1,9 +1,9 @@
 %% Model configuration
 
 
-w = 200;    % [kg]
-h = 0.01;   % [m]
-t = 1;      % [m]
-
-
+w = 200;    % Vehicle weight [kg]
+wn = 1;     % Nominal weight [%]
+h = 0.01;   % Center of Gravity (CG) height [m]
+t = 1;      % Vehicle width (tire center to tire center)[m]
+l = 2;      % Vehicle length (tire center to tire center)[m]
 
