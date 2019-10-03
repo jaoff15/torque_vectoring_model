8fa874165c684508fc291f7b59ebeaacf1b28de8
jaoff15 <jaoff15@student.sdu.dk>
2019-10-03 21:00:50 +0200

    Dynamic model added

---
+++ dynamic_bicycle_model/config_dynamic_bicycle.m
@@ -0,0 +1,11 @@
+
+init_states = [0,0,0,0,0,0];
+
+mass        = 200; % mass [kg]
+yaw_inertia = 100; % 
+
+length_front = 1; % [m]
+length_rear  = 1; % [m]
+
+cornering_stiffness_front = 100;
+cornering_stiffness_rear  = 100;
--- kinematic_bicycle_model/config_kinematic_bicycle.m
+++ kinematic_bicycle_model/config_kinematic_bicycle.m
@@ -5,5 +5,6 @@ length_front = 1;    % Length from rear  axle to CG[m]
 length_rear = 1;    % Length from front axle to CG[m]
 velocity_angle = 0;
 
+
 % [x, y, yaw]
-init_states = [0; 0; 0;0];
+init_states = [0; 0; 0; 0; 0];
