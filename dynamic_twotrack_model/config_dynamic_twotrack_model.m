e0a6fe7a08ecf502d950835cce158fc4f8819a7d
jaoff15 <jaoff15@student.sdu.dk>
2019-10-23 18:14:04 +0200

    Dynamic twotrack model added

---
+++ dynamic_twotrack_model/config_dynamic_twotrack_model.m
@@ -0,0 +1,15 @@
+clc, clear all, format compact
+
+% Lengths
+length_front        = 1; 
+length_rear         = 1; 
+length_width        = 0.5; 
+
+% Cornering stiffnesses
+cornering_stiffness = 5000;
+mass                = 200;
+yaw_inertia         = 1000;
+
+
+
+init_states = [0; 0; 0; 0; 0; 0];
--- kinematic_bicycle_model/config_kinematic_bicycle.m
+++ kinematic_bicycle_model/config_kinematic_bicycle.m
@@ -1,10 +1,10 @@
 clc, clear all, format compact
 
 
-length_front = 1;    % Length from rear  axle to CG[m]
-length_rear = 1;    % Length from front axle to CG[m]
-velocity_angle = 0;
+length_front = 0.8;    % Length from rear  axle to CG[m]
+length_rear = 1.2;    % Length from front axle to CG[m]
+% velocity_angle = 1;
 
 
 % [x, y, yaw]
-init_states = [0; 0; 0; 0];
+init_states = [0; 0; 0; 0; 0;0;0];
